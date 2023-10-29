b90c4701119ef2e5a57244872d4f7b9756561edb
Vishu Tupili <vtupili@Vishus-MacBook.local>
2023-10-29 15:15:09 -0400

    JavaScript

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,8 +1,8 @@
 import os
 import pytest
 from pathlib import Path
-
 from project.app import app, init_db
+import json
 
 # configuration
 DATABASE = "flaskr.db"
@@ -62,3 +62,8 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
+def test_delete_message(client):
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 1
